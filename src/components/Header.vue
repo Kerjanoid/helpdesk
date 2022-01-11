c937d9cf5a9eab008eb70195835f03acfe2c97be
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2022-01-11 23:40:27 +0300

    fix: rewrite Header nav-links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,18 @@
   <div>
     <b-navbar toggleable="md" type="dark" variant="info">
       <b-container fluid="md">
-        <b-navbar-brand href="/tickets">HelpDesk</b-navbar-brand>
+        <b-navbar-brand>HelpDesk</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="position-right">
-            <b-nav-item href="/">New ticket</b-nav-item>
-            <b-nav-item href="/tickets">My tickets</b-nav-item>
+            <b-nav-item
+              @click="changePath('/')"
+              :active="checkPath('/')">New ticket</b-nav-item>
+            <b-nav-item
+              @click="changePath('/tickets')"
+              :active="checkPath('/tickets')">My tickets</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
@@ -20,10 +24,18 @@
 <script>
 export default {
   name: 'Header',
+  methods: {
+    changePath(path) {
+      return this.$router.push(path).catch(() => { });
+    },
+    checkPath(path) {
+      return (this.$route.path === path);
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style>
 .position-right {
   margin-left: auto;
 }
