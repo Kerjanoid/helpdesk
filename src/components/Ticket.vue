88e4635dff32cd6b0403b7ea93eaf7c9f1169a0e
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2022-01-11 23:40:55 +0300

    feat: add Tickets layout

---
+++ src/components/Ticket.vue
@@ -0,0 +1,45 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data() {
+    return {
+      form: {
+        title: '',
+        description: '',
+        files: [],
+      },
+      show: true,
+    };
+  },
+  methods: {
+    formatNames(files) {
+      return files.length === 1 ? files[0].name : `${files.length} files selected`;
+    },
+    handleFileUpload(event) {
+      this.file = event.target.files;
+    },
+    onSubmit(event) {
+      event.preventDefault();
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.form.title = '';
+      this.form.description = '';
+      this.form.files = [];
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Tickets.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <b-container fluid="md">
+      <h2 class="tickets__header">Tickets</h2>
+      <b-form-input class="search" placeholder="Enter search text"></b-form-input>
+      <b-card no-body>
+        <b-tabs pills card>
+          <b-tab title="All" active>
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf1
+            </b-alert>
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf2
+            </b-alert>
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf3
+            </b-alert>
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf1
+            </b-alert>
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf2
+            </b-alert>
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf3
+            </b-alert>
+          </b-tab>
+          <b-tab title="Opened">
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf1
+            </b-alert>
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf2
+            </b-alert>
+            <b-alert
+              show
+              variant="success"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf3
+            </b-alert>
+          </b-tab>
+          <b-tab title="Closed">
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf1
+            </b-alert>
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf2
+            </b-alert>
+            <b-alert
+              show
+              variant="secondary"
+            >
+              Lorem ipsum dolor sit amet, consectetur aa avvdvadva asaf3
+            </b-alert>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// import Form from '@/components/Form.vue';
+
+export default {
+  name: 'Tickets',
+  components: {
+
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped>
+.tickets__header {
+  margin: 30px 0;
+  font-weight: 600;
+}
+.search {
+  margin-bottom: 20px;
+}
+</style>
